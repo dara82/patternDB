72de91b6a88f7f74739282a25314561e7cd00e50
steffi <hans@schuheggers.de>
2017-09-26 10:32:42 +0200

    Test steffi

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Main file index.php
  */
-
+//Test Steffi 
 /* Set some constants, the config file and the autoloader machanism. */
 defined('BASEPATH') or define('BASEPATH', realpath(dirname(__FILE__)));
 require_once(BASEPATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
